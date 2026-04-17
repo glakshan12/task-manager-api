af09476dd444cdd8d9f29acb5713614b954bf655
glakshan12 <lakshgm12@gmail.com>
2026-04-17 14:37:40 +0530

    Fix indentation in models.py and clean comments

--- tasks/models.py
+++ tasks/models.py
@@ -6,14 +6,13 @@ class Task(models.Model):#table name | it should inherit all the table things li
         ('pending','Pending'),#1 store in db and 2nd view of the user nothing just the python list
         ('completed',"Completed"),
     ]#primary id creates automatically
-    user=models.ForeignKey(User,on_delete=models.CASCADE)#dajngo woluld automatically add the user_id in database
+    user=models.ForeignKey(User,on_delete=models.CASCADE)#django woluld automatically add the user_id in database
     title=models.CharField(max_length=255)
     description=models.TextField(blank=True)
     due_date=models.DateField()
     status=models.CharField(max_length=20,choices=STATUS_CHOICES,default='pending')
     created_at=models.DateTimeField(auto_now_add=True)
-    
-    def __str__(self):
+      def __str__(self):
         return self.title
 #makemigrations are instruction for creating the db - reads the models.py file and create it
 #migrate are django run those instructions on mysql and create the table -read that migrations file and create the table
