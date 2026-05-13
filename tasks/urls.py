e84d218845af015547bb030afa942a5c611403f1
glakshan12 <lakshgm12@gmail.com>
2026-05-13 10:33:42 +0530

    Update urls.py

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import RegisterView, LoginView, TaskListCreateView, TaskDetailView
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('tasks/', TaskListCreateView.as_view(), name='task-list-create'),
-    path('tasks/<int:pk>/', TaskDetailView.as_view(), name='task-detail'),
+    path('register/', RegisterView.as_view(), name='register'),#for register
+    path('login/', LoginView.as_view(), name='login'),#for login
+    path('tasks/', TaskListCreateView.as_view(), name='task-list-create'),#for create and view
+    path('tasks/<int:pk>/', TaskDetailView.as_view(), name='task-detail'),# for delete, retrive and update
 ]
